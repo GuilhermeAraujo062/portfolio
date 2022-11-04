fd66d7ef89398625e9b42a85fe4a0b5853e119e9
Guilherme Araujo <99432207+GuilhermeAraujo062@users.noreply.github.com>
2022-11-04 19:52:43 -0300

    Finalizado

--- js/script.js
+++ js/script.js
@@ -1,28 +1,3 @@
-//animação 
-const target = document.querySelectorAll('[data-anime');
-const animationClass = 'animate';
-
-function animeScroll() {
-    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
-    target.forEach(function(element) {
-        if((windowTop) > element.offsetTop) {
-            element.classList.add(animationClass);
-        } else {
-            element.classList.remove(animationClass);
-        }
-    })
-}
-
-animeScroll();
-
-if(target.length) {
-    window.addEventListener('scroll', function(){
-    animeScroll();
-})  
-}
-
-// 
-
 // menu burguer
 
 function clickMenu() {
@@ -30,5 +5,13 @@ function clickMenu() {
         itens.style.display = 'block';
     } else {
         itens.style.display = 'none';
+    }
+}
+
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        itens.style.display = 'block'
+    } else {
+        itens.style.display = 'none'
     }
 }
