d9caa9ca40c04404276e57dfcd2025cfba55fcea
Guilherme Araujo <99432207+GuilhermeAraujo062@users.noreply.github.com>
2022-11-04 19:58:36 -0300

    correção de bugs

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 // menu burguer
 
-function clickMenu() {
-    if (itens.style.display == 'none') {
-        itens.style.display = 'block';
-    } else {
-        itens.style.display = 'none';
-    }
-}
-
 function mudouTamanho() {
     if (window.innerWidth >= 768) {
         itens.style.display = 'block'
     } else {
         itens.style.display = 'none'
+    }
+}
+
+function clickMenu() {
+    if (itens.style.display == 'none') {
+        itens.style.display = 'block';
+    } else {
+        itens.style.display = 'none';
     }
 }
