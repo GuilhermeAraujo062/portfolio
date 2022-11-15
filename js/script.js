08fa9f87d70355a75dcb503a670583de8bdd1435
Guilherme Araujo <99432207+GuilhermeAraujo062@users.noreply.github.com>
2022-11-15 20:12:53 -0300

    correçao do header

--- js/script.js
+++ js/script.js
@@ -1,56 +1,54 @@
 // menu burguer
 
 function mudouTamanho() {
-    if (window.innerWidth >= 768) {
-        itens.style.display = 'block'
-    } else {
-        itens.style.display = 'none'
-    }
+	if (window.innerWidth >= 768)
+		itens.style.display = 'block'
+	else
+		itens.style.display = 'none'
 }
 
 function clickMenu() {
-    if (itens.style.display == 'none') {
-        itens.style.display = 'block';
-    } else {
-        itens.style.display = 'none';
-    }
+	if (itens.style.display == 'none') 
+		itens.style.display = 'block';
+	else 
+		itens.style.display = 'none';
 }
 
 // Animação
 
-const debounce = function(func, wait, immediate) {
-    let timeout;
-    return function(...args) {
-      const context = this;
-      const later = function () {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      const callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
-    };
-  };
-  
-  const target = document.querySelectorAll('[data-anime]');
-  const animationClass = 'animate';
-  
-  function animeScroll() {
-    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
-    target.forEach(function(element) {
-      if((windowTop) > element.offsetTop) {
-        element.classList.add(animationClass);
-      } else {
-        element.classList.remove(animationClass);
-      }
-    })
-  }
-  
-  animeScroll();
-  
-  if(target.length) {
-    window.addEventListener('scroll', debounce(function() {
-      animeScroll();
-    }, 200));
-  }
+const debounce = function (func, wait, immediate) {
+	let timeout;
+	return function (...args) {
+		const context = this;
+		const later = function () {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		const callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll() {
+	const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+	target.forEach(function (element) {
+		if ((windowTop) > element.offsetTop) {
+			element.classList.add(animationClass);
+		} else {
+			element.classList.remove(animationClass);
+		}
+	})
+}
+
+animeScroll();
+
+if (target.length) {
+	window.addEventListener('scroll', debounce(function () {
+		animeScroll();
+	}, 200));
+}
